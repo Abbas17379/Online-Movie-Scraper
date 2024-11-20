d996a294e302fab8dc245314fadc4a486883c3b8
Abbas Naqvi <man74@duke.edu>
2024-11-20 15:12:55 -0500

    Milestone 4

--- ourapp/railsapp/db/seeds.rb
+++ ourapp/railsapp/db/seeds.rb
@@ -1,5 +1,3 @@
-# db/seeds.rb
-
 # Destroy all existing movie records (optional, if you want to start fresh)
 Movie.destroy_all
 
@@ -8,27 +6,32 @@ Movie.create!([
   {
     title: 'Harry Potter And The Goblet of Fire',
     link: 'https://www.imdb.com/title/tt0330373/',
-    status: 'Working'
+    status: 'Working',
+    image_url: 'https://imageurl.com/harry_potter.jpg'
   },
   {
     title: 'Joker',
     link: 'https://www.imdb.com/title/tt7286456/',
-    status: 'Broken'
+    status: 'Broken',
+    image_url: 'https://imageurl.com/joker.jpg'
   },
   {
     title: 'Dune',
     link: 'https://www.imdb.com/title/tt1160419/',
-    status: 'Working'
+    status: 'Working',
+    image_url: 'https://imageurl.com/dune.jpg'
   },
   {
     title: 'Inception',
     link: 'https://www.imdb.com/title/tt1375666/',
-    status: 'Working'
+    status: 'Working',
+    image_url: 'https://imageurl.com/inception.jpg'
   },
   {
     title: 'The Dark Knight',
     link: 'https://www.imdb.com/title/tt0468569/',
-    status: 'Working'
+    status: 'Working',
+    image_url: 'https://imageurl.com/dark_knight.jpg'
   }
 ])
 
