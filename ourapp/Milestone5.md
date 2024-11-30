f529d4f0c5a33517848fb34e8571b20dc4d69032
Abbas Naqvi <man74@duke.edu>
2024-11-30 05:27:57 -0800

    Scraper/Milestone 5

---
+++ ourapp/Milestone5.md
@@ -0,0 +1,14 @@
+# Milestone 5
+
+I figured out the scraper WOOOOOO!!! Now all I need to do is set up the dynamic updating of the website database, and get it on a duke domain/Vm because I had to leave class early that day.
+
+Moreover, my app meets all almost all of the 7 requirements. All I need to do now is get it up and running on a public webpage which  I will do during office hours.
+
+I met all my Milestone1 requirements surprisingly. I was able to get the scraper to work, which was the hardest one so that is reassuring. I didn't implement the sign in like you said after we discussed. All that said, I am excited to meet you after break to get this up and running.
+
+Make sure to watch the zoom link to watch how I got the scraper to work. (IT TOOK HOURS)
+
+
+# Zoom links: https://duke.zoom.us/rec/share/uHPZ3iMnnYZkNNNMXMbKd0k3YmoGINzw7TpSUt5CHWJPejFEdwSNJZE5zyx65PM.7f4VyyRjEaNu6beo?startTime=1732964876000
+
+BYEEE!!!
---
+++ ourapp/railsapp/services/link_scraper_service.rb
@@ -0,0 +1,69 @@
+require 'selenium-webdriver'
+require 'nokogiri'
+require 'json'
+require 'fileutils'
+
+class LinkScraperService
+  def initialize(url)
+    @url = url
+  end
+
+  def scrape_and_save
+    # Explicitly set the host IP for ChromeDriver
+    host_ip = '10.0.0.112' # Replace with your actual host machine's IP
+    chrome_driver_url = "http://#{host_ip}:4444"
+    puts "Connecting to ChromeDriver at #{chrome_driver_url}"
+
+    # Configure Selenium WebDriver
+    options = Selenium::WebDriver::Chrome::Options.new
+    driver = Selenium::WebDriver.for(
+      :remote,
+      url: chrome_driver_url, # Use the explicit host IP
+      capabilities: options
+    )
+
+    # Navigate to the URL
+    driver.get(@url)
+
+    # Allow JavaScript to load content
+    sleep 5
+
+    # Fetch the rendered HTML
+    html_content = driver.page_source
+    document = Nokogiri::HTML(html_content)
+
+    # Base URL for relative links
+    base_url = URI.parse(@url).scheme + '://' + URI.parse(@url).host
+
+    # Extract movie names and links
+    movies = document.css('a').map do |link|
+      href = link['href']
+      text = link.text.strip
+      next if href.nil? || text.empty?
+
+      # If the link is relative, prepend the base URL
+      full_link = href.start_with?('/') ? base_url + href : href
+      { name: text, link: full_link }
+    end.compact.uniq
+
+    # Ensure the tmp directory exists
+    tmp_dir = '/app/tmp'
+    puts "Resolved tmp directory: #{tmp_dir}"
+    FileUtils.mkdir_p(tmp_dir)
+
+    # Generate a unique filename with a timestamp
+    timestamp = Time.now.strftime('%Y%m%d_%H%M%S')
+    file_path = File.join(tmp_dir, "scraped_movies_#{timestamp}.json")
+    puts "Resolved file path: #{file_path}"
+
+    # Save movies to JSON file
+    puts "Debug: About to save file to #{file_path}"
+    File.open(file_path, 'w') do |file|
+      file.write(JSON.pretty_generate(movies))
+    end
+
+    driver.quit
+    puts "Scraped and saved #{movies.size} movies to '#{file_path}'."
+    movies
+  end
+end
